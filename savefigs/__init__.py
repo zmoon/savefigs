c15fac73f0a7d4025e06c2262e2d8098376b8f4d
zmoon <zmoon92@gmail.com>
2021-06-07 11:58:26 -0400

    Save dir and fig save stem prefix

--- savefigs/__init__.py
+++ savefigs/__init__.py
@@ -1,1 +1,64 @@
+"""
+Save all open Matplotlib figures.
+"""
+import inspect
+from pathlib import Path
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
 __version__ = "0.1.0"
+
+
+
+# def pickstem(fig: mpl.figure.Figure):
+#     """Pick figure name stem."""
+#     ...
+
+
+# the main function
+def savefigs(
+    *, 
+    save_dir=None,
+    stem_prefix: str = None,
+    savefig_kwargs: dict = None,
+):
+    """Save all open Matplotlib figures.
+    
+    Parameters
+    ----------
+    save_dir : path-like
+        Directory in which to save the figures.
+        Default: current working directory.
+    stem_prefix
+        Prefix applied to all save figures.
+        Default: file stem of the calling file
+        (if called from a script, else no stem prefix).
+    savefig_kwargs
+        Keyword arguments to `matplotlib.figure.Figure.savefig`.
+        Some won't be allowed (TBD...).
+    """
+    
+    if save_dir is None:
+        save_dir = Path.cwd()
+
+    if stem_prefix is None:
+        caller_frame_info = inspect.stack()[1]
+        caller_fn = caller_frame_info.filename
+        if not caller_fn.startswith("<"):  # '<stdin>', '<ipython ...'>, '<string>'
+            stem_prefix = f"{Path(caller_fn).stem}_"
+        else:
+            stem_prefix = ""
+
+    if savefig_kwargs is None:
+        savefig_kwargs = dict(transparent=True, bbox_inches="tight", pad_inches=0.05, dpi=200)
+
+    for num in plt.get_fignums():
+        fig = plt.figure(num)
+        label = fig.get_label()
+        stem_fig = f"fig{fig.number:02d}" if not label else label
+        p_stem = save_dir / f"{stem_prefix}{stem_fig}"
+        fig.savefig(p_stem.with_suffix(".png"), **savefig_kwargs)
+        # if pdf:
+        #     fig.savefig(p_stem.with_suffix(".pdf"), **savefig_kwargs)
--- tests/test_savefigs.py
+++ tests/test_savefigs.py
@@ -1,5 +1,37 @@
-from savefigs import __version__
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+
+from savefigs import __version__, savefigs
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_save_dir(tmpdir):
+    save_dir = Path(tmpdir)
+
+    # Create and save
+    fig = plt.figure()
+    savefigs(save_dir=save_dir)
+    plt.close(fig)
+
+    # Check name
+    figs = list(save_dir.glob("*"))
+    assert len(figs) == 1
+    assert figs[0].name == "test_savefigs_fig01.png"
+
+
+def test_stem_prefix(tmpdir):
+    save_dir = Path(tmpdir)
+
+    # Create and save
+    fig = plt.figure()
+    savefigs(save_dir=save_dir, stem_prefix="hihi")
+    plt.close(fig)
+
+    # Check name
+    figs = list(save_dir.glob("*"))
+    assert len(figs) == 1
+    assert figs[0].name == "hihifig01.png"
