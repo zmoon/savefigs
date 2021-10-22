1fe88e18b196a3c8d81cc389afba0d0af3e53576
Zachary Moon <zmoon92@gmail.com>
2021-10-22 12:16:57 -0400

    Better name detection in IPython contexts (#9)
    * Possible fix for Spyder IPython interactive
    
    * WIP: look for recent files run in IPython history
    
    * Last run file used when IPython/ipykernel calls
    
    to create `stem_prefix`
    
    * Please mypy
    
    * Some log messages about the caller name detection
    
    * Better ipython history search
    
    - connect to current session instead of last session or tailing
      the combined history
    
    * bit of cleanup
    
    * Remove ipython dev dep
    
    since not sure how to do to pytest tests of the interactive
    usage...
    
    * Update changelog

--- savefigs/__init__.py
+++ savefigs/__init__.py
@@ -2,8 +2,11 @@
 Save all open Matplotlib figures
 """
 import inspect
+import logging
 import math
 import os
+import sys
+import tempfile
 import warnings
 from pathlib import Path
 from typing import Any, Dict, Iterable, Optional, Union
@@ -14,11 +17,72 @@ __version__ = "0.1.2"
 
 __all__ = ("savefigs",)
 
+_P_TMP = Path(tempfile.gettempdir())
+"""`Path` for the operating system root temp dir (according to `tempfile.gettempdir()`)."""
+
+logging.basicConfig(stream=sys.stdout)
+logger = logging.getLogger(__name__)
+
 
 # import matplotlib as mpl
 # def pickstem(fig: mpl.figure.Figure):
 #     """Pick figure name stem."""
 #     ...
+
+
+def _get_ipython_last_ran_file() -> Optional[Path]:
+    from IPython import get_ipython
+
+    # Get current ipython session
+    ip = get_ipython()  # will be `None` if no current session
+    hm = ip.history_manager
+    session_info = hm.get_session_info()
+    # ^ tuple: ID, start datetime, end datetime, ...
+    logger.debug(f"ipython session info: {session_info!r}")
+
+    # Grab all commands of the session, including last
+    hist = list(hm.get_range())
+
+    logger.debug(f"last ipython command: {hist[-1]!r}")
+    # ^ Should have `savefigs` in it somewhere
+    #   Also end datetime in the session info should be `None`
+
+    if "savefigs(" not in hist[-1][2]:
+        logger.debug(
+            "`savefigs` appears not to be used in the last command, "
+            "skipping history search for last file."
+        )
+        return None
+
+    # Search backwards in history for the last `runfile` command
+    # TODO: also look for `run` or `%run`?
+    # TODO: check if the run command actually ran successfully?
+    p = None
+    for t in reversed(hist):
+        # tuple: session ID, command #, command (string)
+        cmd = t[2]
+        if cmd.startswith("runfile("):
+            p = Path(cmd.split(", wdir=")[0][9:-1])
+
+    if p is None:
+        logger.info("file not identified in ipython history")
+    else:
+        logger.info(f"file identified in ipython history: {p.as_posix()!r}")
+
+    return p
+
+
+def _caller_is_ipykernel_interactive(fn: str) -> bool:
+    p = Path(fn)
+    # e.g., `$TMP/ipykernel_255368/2963069196.py`
+    return not fn.startswith("<") and (
+        p.parents[0].name.startswith("ipykernel_") and p.parents[1] == _P_TMP
+    )
+
+
+def _caller_is_ipython(fn: str) -> bool:
+    # e.g., '<ipython-input-5-9043666a436b>'
+    return fn.startswith("<ipython")
 
 
 # the main function
@@ -30,6 +94,7 @@ def savefigs(
     savefig_kwargs: Optional[Dict[str, Any]] = None,
     clobber: bool = True,
     noclobber_method: str = "raise",
+    debug: bool = False,
 ) -> None:
     """Save all open Matplotlib figures.
 
@@ -53,7 +118,13 @@ def savefigs(
         Whether to overwrite existing files.
     noclobber_method : {'raise', 'add_num'}
         What to do when `clobber=False`.
+    debug
+        Whether to print info/debug messages (to stdout).
     """
+    if debug:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.WARNING)
 
     if save_dir is None:
         save_dir = Path.cwd()
@@ -64,12 +135,28 @@ def savefigs(
         raise ValueError("`save_dir` must be a directory that exists")
 
     if stem_prefix is None:
+        stem_prefix = ""
+
+        # Attempt to detect caller file
         caller_frame_info = inspect.stack()[1]
-        caller_fn = caller_frame_info.filename
-        if not caller_fn.startswith("<"):  # '<stdin>', '<ipython ...'>, '<string>'
-            stem_prefix = f"{Path(caller_fn).stem}_"
+        caller = caller_frame_info.filename
+        logger.info(f"caller: {caller!r}")
+        if _caller_is_ipykernel_interactive(caller) or _caller_is_ipython(caller):
+            logger.info("caller detected as ipy interactive (ipython or ipykernel)")
+            p_caller = _get_ipython_last_ran_file()
+        elif caller in ["<stdin>", "<string>"]:  # Python REPL or `python -c`
+            logger.info("caller is stdin (Python REPL) or string input (`-c`)")
+            p_caller = None
         else:
-            stem_prefix = ""
+            logger.info("caller detected as not ipy interactive, stdin, nor string")
+            p_caller = Path(caller)
+
+        # Use caller filename stem in stem_prefix if it has been found
+        if p_caller is not None:
+            logger.info(f"caller file: {p_caller.as_posix()!r}")
+            stem_prefix = f"{p_caller.stem}_"
+        else:
+            logger.info("caller file: none (or not detected)")
 
     if formats is None:
         formats = ["png"]
